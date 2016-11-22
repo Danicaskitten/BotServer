ce06224033fa7cbd36273cd2a2e9e0b95a6da425
LorenzoZoia <lollozoia@hotmail.it>
2016-11-22 10:48:58 +0100

    MOV-39 creating first attempt of parser #time 20m

---
+++ MovieBot/MovieBot/Utility/MessageTestParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Connector;
+
+namespace MovieBot.Utility
+{
+    public static class MessageTestParser
+    {
+        public static void startParsing(ConnectorClient connector, Activity activity)
+        {
+
+        }
+    }
+}
