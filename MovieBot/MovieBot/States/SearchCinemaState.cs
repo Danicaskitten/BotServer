bde04c25e804f800f9a18e669dab29aee4f73658
LorenzoZoia <lollozoia@hotmail.it>
2016-11-24 16:57:10 +0100

    MOV-39  added the stateReply class that is the object used by all the stateController to comunicate and almost finish the SearchCinemaState. TODO implement the connection #time 2h

--- MovieBot/MovieBot/States/SearchCinemaState.cs
+++ MovieBot/MovieBot/States/SearchCinemaState.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieBot.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,47 @@ namespace MovieBot.States
 {
     public class SearchCinemaState
     {
+        private string _channelType;
+        private int _userID;
+        private string _choosenTitle;
+        private string _choosenCinema;
 
+        public string TitleProperty
+        {
+            set { _choosenTitle = value; }
+        }
+        public string CinemaProperty
+        {
+            set { _choosenCinema = value; }
+        }
+        public int UserIDProperty
+        {
+            get { return _userID; }
+        }
+        public int ChannelTypeProperty
+        {
+            get { return _userID; }
+        }
+
+        public SearchCinemaState(string channelType, int userID)
+        {
+            _channelType = channelType;
+            _userID = userID;
+        }
+
+        public StateReply getReplay(string userInput)
+        {
+            if(string.IsNullOrEmpty(userInput))
+            {
+                string replayMessage = "Cool! Tell me which film do you like to see";
+                StateReply replay = new StateReply(false, replayMessage);
+                return replay;
+            }
+            if (_choosenTitle == null)
+            {
+                //TODO sistemare le connessioni
+            }
+            return null;
+        }
     }
 }
---
+++ MovieBot/MovieBot/Utility/StateReply.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieBot.Utility
+{
+    public class StateReply
+    {
+        private bool _finalState;
+        private string _replayMessage;
+
+        public StateReply(bool isFinalState, string replayMessage)
+        {
+            _finalState = isFinalState;
+            _replayMessage = replayMessage;
+        }
+
+        public string GetReplayMessage
+        {
+            get { return _replayMessage; }
+        }
+        public bool IsFinalState
+        {
+            get { return _finalState; }
+        }
+    }
+}
