7262053754700a708316c7ccc1f696637e7b8dba
LorenzoZoia <lollozoia@hotmail.it>
2017-01-07 15:33:51 +0100

    MOV-227 changed the BOT Design in order to make it smarter #time 30m

--- MovieBot/MovieBot/ReplyManagers/SearchCinemaReplyManager.cs
+++ MovieBot/MovieBot/ReplyManagers/SearchCinemaReplyManager.cs
@@ -24,43 +24,17 @@ namespace MovieBot.ReplyManagers
             };
 
             StateReply stateReplay = state.getReplay(input);
-            if(stateReplay != null)
+            Activity replyToConversation = new Activity();
+            if (stateReplay != null)
             {
                 StateClient stateClient = activity.GetStateClient();
-                if (!(stateReplay.IsFinalState))
-                {
-                    BotData userData = await stateClient.BotState.GetUserDataAsync(activity.ChannelId, activity.From.Id);
-                    userData.SetProperty<bool>("SearchCinema", true);
-
-                    userData.SetProperty<SearchCinemaState>("SearchCinemaState", state);
-                    BotData response = await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
-                }
-                else
-                {
-                    await stateClient.BotState.DeleteStateForUserAsync(activity.ChannelId, activity.From.Id);
-                }
-
-                Activity replyToConversation = activity.CreateReply(stateReplay.GetReplayMessage);
-
-                if (stateReplay.GetSpecial == "herocard")
-                {
-                    HeroCard heroGet = stateReplay.HeroCard;
-                    replyToConversation.Recipient = activity.From;
-                    replyToConversation.Type = "message";
-                    replyToConversation.Attachments = new List<Attachment>();
-
-                    Attachment plAttachment = heroGet.ToAttachment();
-                    replyToConversation.Attachments.Add(plAttachment);
-                }
-
-                return replyToConversation;
+                replyToConversation = await this.parseStateReply<SearchCinemaState>(stateReplay, stateClient, state);
             }
             else
             {
-                Activity replyToConversation = activity.CreateReply("Something went wrong in the parsing of your last message. Please try sending again your request");
-                return replyToConversation;
+                replyToConversation = activity.CreateReply("Something went wrong in the parsing of your last message. Please try sending again your Search Cinema request"); 
             }
-            
+            return replyToConversation;
         }
 
         public override async Task<Activity> getResponseWithState<T>(T stateInput)
@@ -70,45 +44,21 @@ namespace MovieBot.ReplyManagers
                 T temp = (T)(object)stateInput;
                 SearchCinemaState state = (SearchCinemaState)(object)stateInput;
                 StateReply stateReplay = state.getReplay(input);
+                Activity replyToConversation = new Activity();
                 if (stateReplay != null)
                 {
                     StateClient stateClient = activity.GetStateClient();
-                    if (!(stateReplay.IsFinalState))
-                    {
-                        BotData userData = await stateClient.BotState.GetUserDataAsync(activity.ChannelId, activity.From.Id);
-                        userData.SetProperty<bool>("SearchCinema", true);
-                        
-                        userData.SetProperty<SearchCinemaState>("SearchCinemaState", state);
-                        BotData response = await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
-                    }
-                    else
-                    {
-                        await stateClient.BotState.DeleteStateForUserAsync(activity.ChannelId, activity.From.Id);
-                    }
-
-                    Activity replyToConversation = activity.CreateReply(stateReplay.GetReplayMessage);
-
-                    if (stateReplay.GetSpecial == "herocard")
-                    {
-                        HeroCard heroGet = stateReplay.HeroCard;
-                        replyToConversation.Recipient = activity.From;
-                        replyToConversation.Type = "message";
-                        replyToConversation.Attachments = new List<Attachment>();
-
-                        Attachment plAttachment = heroGet.ToAttachment();
-                        replyToConversation.Attachments.Add(plAttachment);
-                    }
-                    return replyToConversation;
+                    replyToConversation = await this.parseStateReply<SearchCinemaState>(stateReplay, stateClient, state);
                 }
                 else
                 {
-                    Activity replyToConversation = activity.CreateReply("Something went wrong in the message parsing, please try to restart your request");
-                    return replyToConversation;
+                    replyToConversation = activity.CreateReply("Something went wrong in the parsing of your last message. Please try sending again your Search Cinema request");
                 }
+                return replyToConversation;
             }
             else
             {
-                Activity replyToConversation = activity.CreateReply("I'm so sorry, I didn't manage to process your request");
+                Activity replyToConversation = activity.CreateReply("I'm so sorry, I didn't manage to process your request. Please try sending again your Search Cinema request");
                 return replyToConversation;
             }
         }
